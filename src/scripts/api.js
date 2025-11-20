76c4996cc083c5882fa52f699f589c66a995dd60
Jon4sSantos <jok4gegithub@gmail.com>
2025-11-19 21:44:01 -0300

    apikey

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,6 +1,6 @@
 const search_btn = document.getElementById('search_movies_button');
 const search_input = document.getElementById('search_movies_input');
-const api_Key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmZkYzAxYWI1ODk2NjIwMTc4NmI2NzAwYzk3Y2ZkNCIsIm5iZiI6MTc1NDIzMTAxNC43NzIsInN1YiI6IjY4OGY3MGU2Y2E0ZmVjZmU1M2IyMmQ5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ixZ4j-9Yzwn8BoYKwTp1BW8Z2RKsS0s6d_oRH4bLqyU"; 
+const api_Key = "..."; 
 
 search_btn.addEventListener('click', (e)=> {
      e.preventDefault();
