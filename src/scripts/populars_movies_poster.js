a7d69e54e8937744d745874a8e895cd140a2b59d
Jon4sSantos <jok4gegithub@gmail.com>
2026-02-15 10:17:38 -0300

    Ajeitei o Overlay do menu de filtros, tbm ajeitei o slider, Usei o gpt pra isso

--- src/scripts/populars_movies_poster.js
+++ src/scripts/populars_movies_poster.js
@@ -1,52 +1,24 @@
 const poster_container = document.querySelectorAll('.popular-poster-div');
-const poster = document.querySelectorAll('.popular-poster');
+const posterImg = document.querySelectorAll('.popular-poster');
 const poster_info = document.querySelectorAll('.popular-poster-info');
 const poster_area = document.getElementById('populares_posters');
 
-let posterAberto = false;
-
-poster_container.forEach((posterDiv, i) => {
-     poster[i].addEventListener('mouseenter', () => {
-          if (!posterAberto)
-               poster[i].style.transform = '';
-     });
-     poster[i].addEventListener('mouseleave', () => {
-          if (!posterAberto) {
-               poster[i].style.transform = ' ';
+poster_container.forEach(posterDiv => {
+     posterDiv.addEventListener('click', () => {
+          /*
+          if (posterDiv.classList.contains('active')) {
+               posterDiv.classList.toggle('active');
           }
-     });
-     poster[i].addEventListener('click', () => {
-          if (!posterAberto) {
-               posterAberto = true;
-               openPoster(i);
-          } else {
-               posterAberto = false;
-               closePoster(i);
-          }
-     });
-     poster_info[i].addEventListener('click', () => {
-          posterAberto = false;
-          closePoster(i);
+          else {
+                posterDiv.classList.toggle('active');
+          }*/
+          poster_container.forEach(poster => {
+               if (poster === posterDiv) {
+                    poster.classList.toggle('active');
+               }
+               else {
+                    poster.classList.toggle('hidden');
+               }
      });
 });
-
-function openPoster(i) {
-     poster_area.style.gridTemplateRow = '1fr'; // abre as info
-     poster_container.forEach(posterDiv2 => {
-          posterDiv2.classList.add('hidden'); // os outros somem
-     });
-     poster_container[i].classList.remove('hidden'); // o atual não some
-     poster_container[i].classList.add('active'); // o atual aparece
-     poster[i].style.transform = '' ; // deixa o poster grande
-     poster_info[i].style.display = 'flex'; // aparece as  info
-}
-
-function closePoster(i) {
-     poster_info[i].style.display = ''; // info some
-     poster_area.style.gridTemplateColumns = ''; // aparece outros poster
-     poster[i].style.transform = ' '; //deixa pequeno
-     poster_container.forEach(posterDiv2 => { // para cada container de poster
-          posterDiv2.classList.remove('hidden'); // eles não estao escondidos
-          posterDiv2.classList.remove('active'); // eles não estao ativos
-     });
-}
+});
