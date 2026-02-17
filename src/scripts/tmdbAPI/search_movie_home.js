72a3c146cbd748df9d5d3b693a33d9c4b53f2288
Jon4sSantos <jok4gegithub@gmail.com>
2026-02-17 16:18:41 -0300

    Dei uma estudada no código e agora quero usar a api no slide

--- src/scripts/tmdbAPI/search_movie_home.js
+++ src/scripts/tmdbAPI/search_movie_home.js
@@ -1,27 +1,29 @@
 const searchButton = document.getElementById("search_movies_button");
 const posterPicture = document.getElementById("picture_poster_home");
+const movieTitle = document.getElementById("movie_title");
+const movieOverview = document.getElementById("movie_overview");
 
 const API_BASE = "https://api.themoviedb.org/3";
 const API_KEY = "dbfdc01ab58966201786b6700c97cfd4";
 
 searchButton.addEventListener("click", (event) => {
     event.preventDefault(); // impede o form de recarregar a página
-
+    
     const movieName = document.getElementById("search_movies_input").value.trim();
 
     if (!movieName) {
         alert("Digite o nome de um filme");
         return;
     }
-
-    fetch(`${API_BASE}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(movieName)}&include_adult=false&language=en-US&page=1`)
+/* https://api.themoviedb.org/3/search/movie?api_key=dbfdc01ab58966201786b6700c97cfd4&query=shrek&include_adult=false&language=en-US&page=1 */
+    fetch(`${API_BASE}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(movieName)}&include_adult=false&language=pt-BR&page=1`)
         .then((response) => response.json())
         .then((data) => {
             if (!data.results || data.results.length === 0) {
                 alert("Filme não encontrado");
                 return;
             }
-
+            movieName.value = '';
             const movie = data.results[0];
 
             if (!movie.poster_path) {
@@ -38,6 +40,8 @@ searchButton.addEventListener("click", (event) => {
             posterMovie.alt = movie.original_title;
             posterMovie.id = "poster_home";
             posterPicture.appendChild(posterMovie);
+            movieTitle.innerHTML = `${movie.original_title} - ${movie.release_date}`;
+            movieOverview.innerHTML = movie.overview;
         })
         .catch((error) => console.error("Erro na requisição:", error));
 });
